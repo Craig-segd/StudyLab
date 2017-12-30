dc5d28264e94bab86eda06ff500b799e79938b3f
Craig Taylor <c-r-a-i-g-t13@hotmail.com>
2017-12-30 15:12:43 +0000

    Release 3.0

--- StudyLab/Migrations/201712240249576_TestQuestions.cs
+++ StudyLab/Migrations/201712240249576_TestQuestions.cs
@@ -9,7 +9,7 @@ namespace StudyLab.Migrations
             Sql(@"SET IDENTITY_INSERT [dbo].[Questions] ON
 INSERT INTO[dbo].[Questions]([Id], [QuestionText], [AnswerText], [TypeId]) VALUES(1, N'What is JavaScript?', N'A programming language', 1)
                 INSERT INTO[dbo].[Questions]([Id], [QuestionText], [AnswerText], [TypeId]) VALUES(2, N'What is ReactJS?', N'A JavaScript library.', 3)
-                INSERT INTO[dbo].[Questions]([Id], [QuestionText], [AnswerText], [TypeId]) VALUES(3, N'What is WebAPI', N'A cross-platform framework. ', 4)
+                INSERT INTO[dbo].[Questions]([Id], [QuestionText], [AnswerText], [TypeId]) VALUES(3, N'What is WebAPI?', N'A cross-platform framework. ', 4)
                 INSERT INTO[dbo].[Questions] ([Id], [QuestionText], [AnswerText], [TypeId]) VALUES(4, N'What is MVC?', N'An architectural design pattern.', 2)
 SET IDENTITY_INSERT [dbo].[Questions] OFF");
         }
