747c2abb3f38cbc8d609d63b229de17f2fae1674
Craig Taylor <c-r-a-i-g-t13@hotmail.com>
2017-12-31 22:27:01 +0000

    Added SSL but maybe problematic. Fixed user validation on remove items.

--- StudyLab/App_Start/FilterConfig.cs
+++ StudyLab/App_Start/FilterConfig.cs
@@ -7,6 +7,7 @@ namespace StudyLab
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
